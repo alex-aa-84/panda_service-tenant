e08951a8fd9b2763be976ef9147963a39f265b8d
alex-aa-84 <digitaltrendspy@gmail.com>
2022-05-20 14:21:52 -0400

    modified TenantController

--- src/main/java/wwf/org/tenant/controller/TenantController.java
+++ src/main/java/wwf/org/tenant/controller/TenantController.java
@@ -11,7 +11,7 @@ import wwf.org.tenant.service.TenantService;
 
 import javax.validation.Valid;
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value="/tenants")
 public class TenantController {
