1ce668152a9bd883b2b71e55740c67d4bcb7e72c
alex-aa-84 <digitaltrendspy@gmail.com>
2022-05-31 08:11:24 -0400

    Modelo y Repositorio Imagen

--- src/main/java/wwf/org/tenant/controller/ImageConfigController.java
+++ src/main/java/wwf/org/tenant/controller/ImageConfigController.java
@@ -5,10 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.server.ResponseStatusException;
 import wwf.org.tenant.entity.ImageConfig;
 import wwf.org.tenant.service.ImageConfigService;
 import wwf.org.tenant.serviceApi.FormatMessage;
@@ -43,18 +41,16 @@ public class ImageConfigController {
     }
 
     @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<ImageConfig> uploadImage(@RequestParam("imageFile") MultipartFile file, BindingResult result) throws IOException {
+    public ResponseEntity<ImageConfig> uploadImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
 
         ImageConfig img = new ImageConfig();
         img.setImage(compressBytes(file.getBytes()));
         img.setType(file.getContentType());
         img.setName(file.getOriginalFilename());
 
-        if(result.hasErrors()){
-            throw  new ResponseStatusException(HttpStatus.BAD_REQUEST, formatMessage.format(result));
-        }
+        ImageConfig imgR = imageConfigService.createImage(img);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(imageConfigService.createImage(img));
+        return ResponseEntity.status(HttpStatus.CREATED).body(imgR);
     }
 
     @DeleteMapping(value = "/{id}")
