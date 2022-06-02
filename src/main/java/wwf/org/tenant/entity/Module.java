3370ca0e07bad9a9f72fbae0f48781d54eff8b68
alex-aa-84 <digitaltrendspy@gmail.com>
2022-06-02 19:22:32 -0400

    module eliminacion cascade foto

--- src/main/java/wwf/org/tenant/entity/Module.java
+++ src/main/java/wwf/org/tenant/entity/Module.java
@@ -24,7 +24,7 @@ public class Module {
 
     private String description;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(referencedColumnName = "id")
     @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
