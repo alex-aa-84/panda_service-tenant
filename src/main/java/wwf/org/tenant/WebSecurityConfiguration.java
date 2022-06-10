39244d16a227368a699264b03842729d43cad707
alex-aa-84 <digitaltrendspy@gmail.com>
2022-06-10 14:36:28 -0400

    configuration de http security

---
+++ src/main/java/wwf/org/tenant/WebSecurityConfiguration.java
@@ -0,0 +1,22 @@
+package wwf.org.tenant;
+
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        // @formatter:off
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .and();
+        // @formatter:on
+    }
+}
