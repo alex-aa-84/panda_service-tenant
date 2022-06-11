a90c2ee76e49cbb770f7f7ee888dfc0837726fad
alex-aa-84 <digitaltrendspy@gmail.com>
2022-06-11 16:49:35 -0400

    modify websecurity

--- src/main/java/wwf/org/tenant/WebSecurityConfiguration.java
+++ src/main/java/wwf/org/tenant/WebSecurityConfiguration.java
@@ -1,14 +1,18 @@
 package wwf.org.tenant;
 
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Configuration
 @EnableWebSecurity
-@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
