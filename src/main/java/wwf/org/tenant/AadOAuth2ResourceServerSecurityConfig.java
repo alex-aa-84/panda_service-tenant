f2628782b66c3db1763b35bf6b21406ea4fe45c9
alex-aa-84 <digitaltrendspy@gmail.com>
2022-06-12 09:56:58 -0400

    modified security

---
+++ src/main/java/wwf/org/tenant/AadOAuth2ResourceServerSecurityConfig.java
@@ -0,0 +1,20 @@
+package wwf.org.tenant;
+
+import com.azure.spring.cloud.autoconfigure.aad.AadResourceServerWebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AadOAuth2ResourceServerSecurityConfig extends AadResourceServerWebSecurityConfigurerAdapter {
+
+    /**
+     * Add configuration logic as needed.
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+    }
+}
